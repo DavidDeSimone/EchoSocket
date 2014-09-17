9f2212d9b489096082031362e5d62196f7e4ec5e
David DeSimone <djd231@scarletmail.rutgers.edu>
2014-09-17 01:22:48 -0400

    Fixed object responsible for output from a BufferedWriter to a PrintWriter. Moved termination character check before sending input

--- src/EchoClient.java
+++ src/EchoClient.java
@@ -1,11 +1,9 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
 
 /*@author David DeSimone
  * Basic Echo client for Java. Read's users input from System.in and echos it to a server.
@@ -15,88 +13,75 @@ import java.net.UnknownHostException;
  */
 public class EchoClient {
 
-	
-	//The socket object we will be communicating over
+	// The socket object we will be communicating over
 	public static Socket sock;
-	
+
 	public static BufferedReader clientInput;
 	public static BufferedReader serverInput;
-	public static BufferedWriter clientRes;
-	
+	public static PrintWriter clientRes;
+
 	public static void main(String[] args) {
-		
-		
-		
-		//Host name and port to be connected to
+		// Host name and port to be connected to
 		String hostName;
 		String port;
 		int portNum;
-		
-		
-		
+
 		String cIn;
 		String cOut;
-		
-		if(args.length != 2) {
+
+		if (args.length != 2) {
 			System.out.println("Error, not enough arguments");
 			return;
 		}
-		
-		
+
 		hostName = args[0];
 		port = args[1];
 		portNum = Integer.parseInt(port);
-		
+
 		try {
-			//Try and open the Socket
+			// Try and open the Socket
 			sock = new Socket(hostName, portNum);
-		
-			//If the socket opens, open the readers/writers
+
+			// If the socket opens, open the readers/writers
 			clientInput = new BufferedReader(new InputStreamReader(System.in));
-			serverInput = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-			clientRes = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
-			
-			//Read the Clients input
-			while((cIn = clientInput.readLine()) != null) {
-				//Send the clients input to the server
-				clientRes.write(cIn);
-				if(cIn.equals("$") || cIn.equals("#")) {
+			serverInput = new BufferedReader(new InputStreamReader(
+					sock.getInputStream()));
+			clientRes = new PrintWriter(sock.getOutputStream(), true);
+
+			// Read the Clients input
+			while ((cIn = clientInput.readLine()) != null) {
+				if (cIn.equals("$") || cIn.equals("#")) {
 					shutDown();
 					return;
 				}
-				
-				//If we are still running, get the servers responce and print it to console
-				while((cOut = serverInput.readLine()) != null) {
-					System.out.println(cOut);
-				}
-				
-				
-				
+				// Send the clients input to the server
+				clientRes.println(cIn);
+
+				// If we are still running, get the servers responce and print
+				// it to console
+				cOut = serverInput.readLine();
+				System.out.println(cOut);
+
 			}
-		
-		
-			
-		} catch(UnknownHostException e) {
-			System.out.println("Error, could not connect to host " + hostName + " on port " + portNum);
+
+		} catch (UnknownHostException e) {
+			System.out.println("Error, could not connect to host " + hostName
+					+ " on port " + portNum);
 			e.printStackTrace();
-		} catch(IOException e) {
-			System.out.println("Error, could not connect to host " + hostName + " on port " + portNum);
+		} catch (IOException e) {
+			System.out.println("Error, could not connect to host " + hostName
+					+ " on port " + portNum);
 			e.printStackTrace();
 		}
-		
-		
-		
-		
-		
+
 	}
-	
+
+	/* Method used to close relevant input stream and socket connection */
 	public static void shutDown() throws IOException {
 		clientInput.close();
 		serverInput.close();
 		clientRes.close();
 		sock.close();
 	}
-	
-	
-	
+
 }
